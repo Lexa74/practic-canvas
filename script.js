7c340818aa016e4e6ed9accb258f97926d4a5cc0
Sealexey <tany--96@mail.re>
2024-02-08 21:58:16 +0300

    refactoring Snake can move Generation eat for snake

--- script.js
+++ script.js
@@ -2,21 +2,16 @@ const canvas = document.getElementById('canvas')
 const scoreHtml = document.getElementById('score')
 const ctx = canvas.getContext('2d')
 
-let isPause = false
 let direction;
 
 const canW = 300;
 const canH = 300;
 
-
-let stepX = 0;
-let stepY = 0;
-
-const speed = 3.6;
-
-
-let score = 0;
-
+const defaultSetting = {
+    isPause: false,
+    score: 0,
+    speed: 1
+}
 
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min) + min)
@@ -44,31 +39,33 @@ const snake = {
     height: 10,
     x: 0,
     y: 0,
+    stepX: 0,
+    stepY: 0,
     center: function () {
         this.x = canW  / 2 - this.height / 2;
         this.y = canH  / 2 - this.width / 2;
     },
     draw: function () {
         ctx.clearRect(this.x, this.y, this.width, this.height)
-        ctx.fillStyle = '#dedede';
-        ctx.fillRect(this.x += stepX, this.y += stepY, this.width, this.height)
+        this.x += snake.stepX
+        this.y += snake.stepY
 
         if(this.x > canW) {
-            ctx.clearRect(this.x, this.y, this.width, this.height)
             this.x = -this.width
         }
         if(this.x < -this.width) {
-            ctx.clearRect(this.x, this.y, this.width, this.height)
             this.x = canW
         }
         if(this.y > canH) {
-            ctx.clearRect(this.x,this.y, this.width, this.height)
             this.y = -this.height
         }
         if(this.y < -this.height) {
-            ctx.clearRect(this.x,this.y, this.width, this.height)
             this.y = canH
         }
+
+        ctx.clearRect(this.x, this.y, this.width, this.height)
+        ctx.fillStyle = '#dedede';
+        ctx.fillRect(this.x += snake.stepX, this.y += snake.stepY, this.width, this.height)
 
         const endRectX = this.x + this.width;
         const endEatX = eat.x + eat.width;
@@ -77,8 +74,8 @@ const snake = {
 
         if((endEatX >= this.x && endRectX >= eat.x) && (endEatY >= this.y && endRectY >= eat.y)) {
             console.log('Съел')
-            score++
-            scoreHtml.innerHTML = score.toString()
+            defaultSetting.score++
+            scoreHtml.innerHTML = defaultSetting.score.toString()
             eat.clear()
             eat.draw()
         }
@@ -86,7 +83,7 @@ const snake = {
 }
 
 function draw() {
-    if(isPause) return;
+    if(defaultSetting.isPause) return;
 
     snake.draw()
     window.requestAnimationFrame(draw)
@@ -95,30 +92,31 @@ function draw() {
 document.addEventListener('keydown', (e) => {
     if(e.key === 'ArrowRight') {
         if(direction === 'ArrowLeft') return
-        stepX = 0.4 + speed
-        stepY = 0
+        snake.stepX = defaultSetting.speed
+        snake.stepY = 0
     }
     if(e.key === 'ArrowLeft') {
         if(direction === 'ArrowRight') return
-        stepX = -speed - 0.4
-        stepY = 0
+        snake.stepX = -defaultSetting.speed
+        snake.stepY = 0
     }
     if(e.key === 'ArrowDown') {
         if(direction === 'ArrowUp') return
-        stepY = 0.4 + speed
-        stepX = 0
+        snake.stepY = defaultSetting.speed
+        snake.stepX = 0
     }
     if(e.key === 'ArrowUp') {
         if(direction === 'ArrowDown') return
-        stepY = -speed - 0.4
-        stepX = 0
+        snake.stepY = -defaultSetting.speed
+        snake.stepX = 0
     }
     if(e.key === ' ') {
-        isPause = !isPause
+        defaultSetting.isPause = !defaultSetting.isPause
         draw()
     }
     direction = e.key
 })
+
 snake.center()
 eat.draw()
 window.requestAnimationFrame(draw)
